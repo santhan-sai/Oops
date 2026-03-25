5a72b27c6849d610f90d4c2078e60c5218b85d1f
santhan-sai <santhansaipallineni@gmail.com>
2026-03-25 13:09:42 +0530

    UC4 DONE

--- OopsBannerApp4.java
+++ OopsBannerApp4.java
@@ -5,7 +5,7 @@ public class OopsBannerApp4 {
 	    line[1]=String.join(" **   **  "," **   **  ","*     * "," **     ");
 		line[2]=String.join("**     ** ","**     ** ","*     * ","**      ");
 		line[3]=String.join("**     ** ","**     ** ","******* ","  ***** ");
-		line[4]=String.join("**     ** ","**     ** ","*       ","      **");
+		line[4]=String.join("**     ** ","**     ** ","*       ","       **");
 		line[5]=String.join(" **   **  "," **   **  ","*       "," **   **");
 		line[6]=String.join("   ***    ","   ***    ","*       ","  ***** ");
 	for(int i=0;i<7;i++){
